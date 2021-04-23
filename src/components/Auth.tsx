c9f676ef359e6d97978616bc60d15b120ca47c1e
nabesuke <nabesuke5098@gmail.com>
2021-04-23 09:02:24 +0900

    [add]add Icon on loginButton with google

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -288,6 +288,7 @@ const Auth: React.FC = () => {
               fullWidth
               variant="contained"
               color="primary"
+              startIcon={<CameraIcon />}
               className={classes.submit}
               onClick={signInGoogle}
             >
