7a690d5d3ad0933dd1cd8f6a5f3df3256557546e
nabesuke <nabesuke5098@gmail.com>
2021-04-24 18:14:33 +0900

    [add]tweet form

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from "./Feed.module.css";
 import { auth } from "../firebase";
 import TweerInput from "./TweerInput ";
 
 const Feed = () => {
   return (
-    <div>
+    <div className={styles.feed}>
       Feed
       <TweerInput />
       <button onClick={() => auth.signOut()}>Logout</button>
