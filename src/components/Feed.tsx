28063ac659246f9c6c4df38404e955321efaba56
nabesuke <nabesuke5098@gmail.com>
2021-04-24 12:31:02 +0900

    [add]TweetInput Avatar module

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { auth } from "../firebase";
+import TweerInput from "./TweerInput ";
 
 const Feed = () => {
   return (
     <div>
       Feed
+      <TweerInput />
       <button onClick={() => auth.signOut()}>Logout</button>
     </div>
   );
